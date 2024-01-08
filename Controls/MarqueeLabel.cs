7dc3b6474cc7bf13c9811ba1b766d87e8dcb75fc
Ronald <r.vries@live.com>
2024-01-08 22:19:57 +0100

    Completed MarqueeLabel

--- Controls/MarqueeLabel.cs
+++ Controls/MarqueeLabel.cs
@@ -1,55 +1,234 @@
 ﻿namespace Months18.Controls;
 
-public class MarqueeLabel : Frame
+public class MarqueeLabel : ScrollView
 {
     public MarqueeLabel()
     {
+        Orientation = ScrollOrientation.Horizontal;
         Padding = new Thickness(0);
         Margin = new Thickness(0);
         BackgroundColor = Colors.Transparent;
+        HorizontalScrollBarVisibility = ScrollBarVisibility.Never;
 
-        innerFrame = new Frame { Padding = new Thickness(0), Margin = new Thickness(0), BackgroundColor = Colors.Transparent, Content = new Label() };
+        _label = new Label { LineBreakMode = LineBreakMode.NoWrap };
+        _label.SetBinding(Label.TextProperty, new Binding(nameof(Text), source: this));
 
-        marqueeLabel = (Label)innerFrame.Content;
-        marqueeLabel.SetBinding(Label.TextProperty, new Binding(nameof(Text), source: this));
-        Content = innerFrame;
-
+        Content = _label;
         SizeChanged += OnSizeChanged;
     }
 
-    private double translationX = 0;
-    private readonly double durationInSeconds = 10; // Adjust as needed
+    double translationX = 0;
+    double totalTextWidth = 0;
+    bool isAnimationRunning = false;
+    bool stopAnimationFlag = false;
+    readonly Label _label;
 
-    readonly Frame innerFrame;
-    readonly Label marqueeLabel;
-
+    #region BindableProperties
     public static readonly BindableProperty TextProperty =
-        BindableProperty.Create(
+       BindableProperty.Create(
         nameof(Text),
         typeof(string),
         typeof(MarqueeLabel),
         string.Empty,
-        propertyChanged: OnTextChanged);
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marquee && marquee._label != null)
+            {
+                marquee._label.Text = (string)newValue;
 
-    public string Text { get { return (string)GetValue(TextProperty); } set { SetValue(TextProperty, value); } }
+                SetTotalTextWidth(marquee);
+            }
+        });
+
+    public static readonly BindableProperty TextColorProperty =
+     BindableProperty.Create(
+        nameof(TextColor),
+        typeof(Color),
+        typeof(MarqueeLabel),
+        default(Color),
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel._label.TextColor = (Color)newValue;
+            }
+        });
+
+    public static readonly BindableProperty SelectedTextColorProperty =
+        BindableProperty.Create(
+        nameof(SelectedTextColor),
+        typeof(Color),
+        typeof(MarqueeLabel),
+        default(Color),
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel.UpdateTextColor();
+            }
+        });
+
+    public static readonly BindableProperty FontFamilyProperty =
+    BindableProperty.Create(
+        nameof(FontFamily),
+        typeof(string),
+        typeof(MarqueeLabel),
+        default(string),
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel._label.FontFamily = (string)newValue;
+            }
+        });
+
+
+    public static readonly BindableProperty FontSizeProperty =
+    BindableProperty.Create(
+        nameof(FontSize),
+        typeof(double),
+        typeof(MarqueeLabel),
+        14.0,
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel._label.FontSize = (double)newValue;
+            }
+        });
+
+    public static readonly BindableProperty IsActiveProperty =
+        BindableProperty.Create(
+        nameof(IsActive),
+        typeof(bool),
+        typeof(MarqueeLabel),
+        true,
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                if((bool)newValue)
+                {
+                    marqueeLabel._label.LineBreakMode = LineBreakMode.NoWrap;
+                    marqueeLabel.AnimateMarquee(); // Start animations when IsActive is true
+                } else
+                {
+                    marqueeLabel.StopAnimation();
+                }
+            }
+        });
+
+    public static readonly BindableProperty IsSelectedProperty =
+        BindableProperty.Create(
+        nameof(IsSelected),
+        typeof(bool),
+        typeof(MarqueeLabel),
+        false,
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel.UpdateTextColor();
+            }
+        });
+
+    public static readonly BindableProperty DurationAnimationProperty =
+       BindableProperty.Create(
+        nameof(DurationAnimation),
+        typeof(double),
+        typeof(MarqueeLabel),
+        6.0, // Default duration for the animation
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel.OnPropertyChanged(nameof(DurationAnimation));
+            }
+        });
+
+    public static readonly BindableProperty DurationPauseProperty =
+        BindableProperty.Create(
+        nameof(DurationPause),
+        typeof(double),
+        typeof(MarqueeLabel),
+        1.0, // Default duration for the pause
+        propertyChanged: (bindable, oldValue, newValue) =>
+        {
+            if(bindable is MarqueeLabel marqueeLabel)
+            {
+                marqueeLabel.OnPropertyChanged(nameof(DurationPause));
+            }
+        });
+
+    public string Text { get => (string)GetValue(TextProperty); set => SetValue(TextProperty, value); }
+
+    public Color TextColor { get => (Color)GetValue(TextColorProperty); set => SetValue(TextColorProperty, value); }
+
+    public Color SelectedTextColor
+    {
+        get => (Color)GetValue(SelectedTextColorProperty);
+        set => SetValue(SelectedTextColorProperty, value);
+    }
+
+    public string FontFamily
+    {
+        get => (string)GetValue(FontFamilyProperty);
+        set => SetValue(FontFamilyProperty, value);
+    }
+
+    public double FontSize { get => (double)GetValue(FontSizeProperty); set => SetValue(FontSizeProperty, value); }
+
+    public bool IsActive { get => (bool)GetValue(IsActiveProperty); set => SetValue(IsActiveProperty, value); }
+
+    public bool IsSelected { get => (bool)GetValue(IsSelectedProperty); set => SetValue(IsSelectedProperty, value); }
+
+    public double DurationAnimation
+    {
+        get => (double)GetValue(DurationAnimationProperty);
+        set => SetValue(DurationAnimationProperty, value);
+    }
+
+    public double DurationPause
+    {
+        get => (double)GetValue(DurationPauseProperty);
+        set => SetValue(DurationPauseProperty, value);
+    }
+    #endregion
 
     private async void AnimateMarquee()
     {
-        await Task.Delay(TimeSpan.FromSeconds(1)); // Adjust the delay duration as needed
+        isAnimationRunning = false;
+        if(stopAnimationFlag == true)
+        {
+            stopAnimationFlag = false;
+            return;
+        }
 
-        // Set up the first animation from 0 to -Width
-        var firstAnimation = new Animation(x =>
+        if(totalTextWidth <= Width)
+            return; // Skip animation if the content fits
+
+        await Task.Delay(TimeSpan.FromSeconds(DurationPause));
+
+        // Set up the first animation from 0 to -totalTextWidth
+        var firstAnimation = new Animation(
+            x =>
             {
                 translationX = x;
-                innerFrame.TranslationX = translationX;
-            }, 0, -Width);
+                _label.TranslationX = translationX;
+            },
+            0,
+            -totalTextWidth,Easing.CubicIn);
 
         // Set up the second animation from Width to 0
-        var secondAnimation = new Animation(x =>
+        var secondAnimation = new Animation(
+            x =>
             {
                 translationX = x;
-                innerFrame.TranslationX = translationX;
-            }, Width, 0, Easing.Linear);
+                _label.TranslationX = translationX;
+            },
+            totalTextWidth,
+            0,
+            Easing.CubicOut);
 
         // Create a sequence of animations
         var sequence = new Animation
@@ -59,24 +238,35 @@ public class MarqueeLabel : Frame
         };
 
         // Callback for the end of the sequence
-        sequence.Commit(this, "MarqueeAnimation", 16, (uint)(durationInSeconds * 1000), Easing.Linear, (value, cancelled) =>
-            {
-                AnimateMarquee(); // Initialize a new set of animations for the next iteration
-            });
+        sequence.Commit(
+            this,
+            "MarqueeAnimation",
+            16,
+            (uint)(DurationAnimation * 1000),
+            Easing.Linear,
+            (value, cancelled) => AnimateMarquee());
     }
 
-    private static void OnTextChanged(BindableObject bindable, object oldValue, object newValue)
+    private void StopAnimation()
     {
-        if(bindable is MarqueeLabel marqueeFrame && marqueeFrame.marqueeLabel != null)
-        {
-            marqueeFrame.marqueeLabel.Text = (string)newValue;
-        }
+        if(isAnimationRunning)
+            stopAnimationFlag = true;
+        else
+            _label.LineBreakMode = LineBreakMode.TailTruncation;
     }
+
+    private void UpdateTextColor() => _label.TextColor = IsSelected && SelectedTextColor != default(Color) ? SelectedTextColor : TextColor;
 
     private void OnSizeChanged(object? sender, EventArgs e)
     {
-        // SizeChanged event triggered, initialize components and unsubscribe from the event
+        SetTotalTextWidth(this);
         AnimateMarquee();
         SizeChanged -= OnSizeChanged;
+    }
+
+    private static void SetTotalTextWidth(MarqueeLabel marquee)
+    {
+        SizeRequest labelSize = marquee._label.Measure(double.PositiveInfinity, double.PositiveInfinity);
+        marquee.totalTextWidth = labelSize.Request.Width;
     }
 }
